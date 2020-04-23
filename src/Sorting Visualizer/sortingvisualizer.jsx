55b97fcf3a23008f1bf83bcfc7f2abe40053d583
DogeWithIt <floarea.raul@gmail.com>
2020-04-23 17:22:53 +0300

    Revert "Revert "Revert "cu mana pe volan in pula o minora merg spre dosar penal cu 200 la ora"""
    This reverts commit 68558410d87089fd38e74ae7ae1f54dd19fc1692.

--- src/Sorting Visualizer/sortingvisualizer.jsx
+++ src/Sorting Visualizer/sortingvisualizer.jsx
@@ -3,7 +3,6 @@ import './sortingvisualizer.css';
 import {Buttons} from './buttons';
 import {countingSort} from '../Algorithms/countingsort';
 import {quicksort} from '../Algorithms/quicksort';
-import {bubblesort} from '../Algorithms/bubblesort';
 import {test} from '../Algorithms/testAlgo';
 
 export class SortingVisualizer extends React.Component{
@@ -14,11 +13,16 @@ export class SortingVisualizer extends React.Component{
         this.shuffle = this.shuffle.bind(this);
         this.cSort = this.cSort.bind(this);
         this.qSort = this.qSort.bind(this);
-        this.bSort = this.bSort.bind(this);
     }
 
     shuffle(){
-        let len = document.getElementById('range').value, rbound = 10000, array1 = [];
+        let len = 100, rbound = 1000, array1 = [];
+        if(window.screen.height <= 1920 
+            && window.screen.width <= 1080){
+                len = 50;
+                rbound = 500;
+        }
+
         for(let i = 0; i < len; i++){
             array1.push(Math.floor(Math.random()*rbound + 1));
         }
@@ -26,48 +30,48 @@ export class SortingVisualizer extends React.Component{
         this.setState({array: array1});
     }
 
-    async cSort(){
+    cSort(){
         let sortedArray = this.state.array;
         let arrayElements = document.getElementsByClassName('numbers');
         const length = arrayElements.length;
         sortedArray = countingSort(this.state.array);
 
         for(let i = 0; i < length; i++){
-            await new Promise(r => setTimeout(r, 10));
-            arrayElements[i].style.backgroundColor = 'red';
+            setTimeout(() => {
+                arrayElements[i].style.backgroundColor = 'red';
+            }, i*10);
         }
         
-        for(let i = 0; i < length; i++){
-            await new Promise(r => setTimeout(r, 10));
-            arrayElements[i].style.height = `${sortedArray[i]/10}px`;
-            arrayElements[i].style.backgroundColor = 'aquamarine';    
-        }
-
-        await new Promise(r => setTimeout(r, 10))
-        this.setState({array: sortedArray});
+        setTimeout(() => {
+            for(let i = 0; i < length; i++){
+                setTimeout(() => {
+                    arrayElements[i].style.height = `${sortedArray[i]}px`;
+                    arrayElements[i].style.backgroundColor = 'aquamarine';
+                }, i*10);
+            }
+        }, length*10);
     }
 
     async qSort(){
-        let animations = [], arrayElements = document.getElementsByClassName('numbers');
-        //let speed = document.getElementById('speed')
+        let animations = [], arrayElements = document.getElementsByClassName('numbers'); //choose the divs that represent the numbers of this.state.array, and declare an empty animations array
         let array1 = this.state.array;
         
-        array1 = quicksort(animations, array1, 0, array1.length-1);
+        array1 = quicksort(animations, array1, 0, array1.length-1); //quicksort found in ../Algorithms/quicksort.js
         
         for(let i = 0; i < animations.length; i++){
-            if(animations[i].compare !== undefined){
+            if(animations[i].compare !== undefined){ //if we have 2 pointers to compare, turn em red for 15ms
                 arrayElements[animations[i].compare[0]].style.backgroundColor = 'red';
                 arrayElements[animations[i].compare[1]].style.backgroundColor = 'red';
 
                 await new Promise(r => setTimeout(r, 10));
-                arrayElements[animations[i].compare[0]].style.backgroundColor = 'aquamarine';
+                arrayElements[animations[i].compare[0]].style.backgroundColor = 'aquamarine';//and make em aquamarine
                 arrayElements[animations[i].compare[1]].style.backgroundColor = 'aquamarine';
             }
-            if(animations[i].swap !== undefined){
+            if(animations[i].swap !== undefined){//if we need to swap 2 values, make the divs red for 15ms and swap them
                 arrayElements[animations[i].swap[0]].style.backgroundColor = 'red';
                 arrayElements[animations[i].swap[1]].style.backgroundColor = 'red';
 
-                let temp = arrayElements[animations[i].swap[0]].style.height; 
+                let temp = arrayElements[animations[i].swap[0]].style.height; //generic swap with temp, should work
                 arrayElements[animations[i].swap[0]].style.height = arrayElements[animations[i].swap[1]].style.height;
                 arrayElements[animations[i].swap[1]].style.height = temp;
 
@@ -76,37 +80,6 @@ export class SortingVisualizer extends React.Component{
                 arrayElements[animations[i].swap[1]].style.backgroundColor = 'aquamarine';
             }
         }
-
-        this.setState({array: array1});
-    }
-
-    async bSort(){
-        let animations = [], sortedArray = this.state.array;
-        let arrayElements = document.getElementsByClassName('numbers');
-        sortedArray = bubblesort(animations, sortedArray);
-
-        for(let i = 0; i < animations.length; i++){
-            if(animations[i].compare !== undefined){
-                arrayElements[animations[i].compare].style.backgroundColor = 'red';
-
-                await new Promise(r => setTimeout(r, 10));
-                arrayElements[animations[i].compare].style.backgroundColor = 'aquamarine';
-            }
-
-            if(animations[i].swap !== undefined){
-                arrayElements[animations[i].swap[0]].style.backgroundColor = 'red';
-                arrayElements[animations[i].swap[1]].style.backgroundColor = 'red';
-
-                let temp = arrayElements[animations[i].swap[0]].style.height; 
-                arrayElements[animations[i].swap[0]].style.height = arrayElements[animations[i].swap[1]].style.height;
-                arrayElements[animations[i].swap[1]].style.height = temp;
-
-                await new Promise(r => setTimeout(r, 10));
-                arrayElements[animations[i].swap[0]].style.backgroundColor = 'aquamarine';
-                arrayElements[animations[i].swap[1]].style.backgroundColor = 'aquamarine';
-            }
-        }
-        this.setState({array: sortedArray});
     }
 
     componentDidMount(){
@@ -121,14 +94,13 @@ export class SortingVisualizer extends React.Component{
                         this.state.array.map((value, index) => (
                             <div className="numbers"
                                 key={index}
-                                style={{height: value/10}}>
+                                style={{height: value}}>
                             </div>
                         ))
                     }
                 </div>
-                <div id="button">
-                    <input type="range" min="5" max="200" id="range" onChange={this.shuffle} />
-                    <Buttons onHandleShuffle={this.shuffle} cSort={this.cSort} qSort={this.qSort} bSort={this.bSort} /*iSort={this.iSort}*//>
+                <div className="button">
+                    <Buttons onHandleShuffle={this.shuffle} cSort={this.cSort} qSort={this.qSort} /*iSort={this.iSort}*//>
                 </div>
             </>
         );
