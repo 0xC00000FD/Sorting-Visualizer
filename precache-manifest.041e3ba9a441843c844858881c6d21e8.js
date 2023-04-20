4ae038b31de96abec2c2fdab250d1e34ed18e5d0
0xC00000FD <floarea.raul@gmail.com>
2023-04-20 23:47:55 +0300

    Updates

---
+++ precache-manifest.041e3ba9a441843c844858881c6d21e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c321b08b5279bd1e566be134405f09aa",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "e0e4a48f01f3330478e0",
+    "url": "/Sorting-Visualizer/static/css/main.ea38d0b5.chunk.css"
+  },
+  {
+    "revision": "501f5e67c9716543a83f",
+    "url": "/Sorting-Visualizer/static/js/2.fce88e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Visualizer/static/js/2.fce88e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e4a48f01f3330478e0",
+    "url": "/Sorting-Visualizer/static/js/main.61c039bb.chunk.js"
+  },
+  {
+    "revision": "3d99faf97218ba2e8b58",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.c7d11081.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Sorting-Visualizer/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Visualizer/index.html/precache-manifest.afb2adc2c590925037b1c9119b138af1.js"
+  "/Sorting-Visualizer/precache-manifest.041e3ba9a441843c844858881c6d21e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Visualizer/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
